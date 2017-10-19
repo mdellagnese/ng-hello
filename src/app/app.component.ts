b95c0755ef900220bc78ecb81cacbe422f7a4ea5
Mauricio Dellagnese <mauricio.dellagnese@gmail.com>
2017-10-19 13:55:38 -0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  post = {
+    title: "Title",
+    isFavorite: true
+  }
+
+  onFavoriteChanged(isFavorite) {
+    console.log("Mudou: " + isFavorite);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
+import { CoursesComponent } from './courses.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule
